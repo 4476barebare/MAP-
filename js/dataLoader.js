720b9b8a2fc6d373befc9b7e58af7111de2b52d7
4476barebare <aroe6295@gmail.com>
2026-04-24 10:30:59 +0900

    Add files via upload

--- js/dataLoader.js
+++ js/dataLoader.js
@@ -89,7 +89,7 @@ function loadLocationCSV(csvUrl) {
             // -----------------------
             // グラフ生成
             // -----------------------
-            buildAreaGraphFromGrid();
+            buildAreaGraphFromGrid(allRows);
 
             return { main, areas, spots };
         });
