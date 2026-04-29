a401639ac3e9e40d18ede522cea0c68b517a5a9a
4476barebare <aroe6295@gmail.com>
2026-04-29 12:54:51 +0900

    Add files via upload

--- js/dataLoader.js
+++ js/dataLoader.js
@@ -244,17 +244,22 @@ function drawLocation(name, lat, lng, zoom, options = {}) {
     const tileUrl = 'https://cyberjapandata.gsi.go.jp/xyz/ort/{z}/{x}/{y}.jpg';
 
     if (window.map) {
-        window.map.flyTo([lat, lng], zoom, { duration: 0.5 });
+        
+       
+       window.map.flyTo([lat, lng], zoom, { duration: 0.5 });
+
+window.map.once('moveend', function () {
 
     if (window.currentTileLayer) {
-            window.map.removeLayer(window.currentTileLayer);
-        }
+        window.map.removeLayer(window.currentTileLayer);
+    }
 
-        window.currentTileLayer =
-    L.tileLayer(tileUrl, {
-        attribution: '© 国土地理院',
-        keepBuffer: 8
-    }).addTo(window.map);
+    window.currentTileLayer =
+        L.tileLayer(tileUrl, {
+            attribution: '© 国土地理院',
+            keepBuffer: 8
+        }).addTo(window.map);
+});
 
         mapOptions.scrollWheelZoom
             ? window.map.scrollWheelZoom.enable()
