7241ea623b128d5d1c4ac539add80036491ebaa5
4476barebare <aroe6295@gmail.com>
2026-05-19 10:16:27 +0900

    Add files via upload

--- js/dataLoader.js
+++ js/dataLoader.js
@@ -1545,21 +1545,17 @@ const adjustWeatherCodeForPop = (code, pop) => {
 };
   // =========================
 
-const labels = [
-  "時間 / 天気",
-  "気温 / 降水 / 確率",
-  "風速 / 風向"
-];
+  const labels = ["TIME","天気","気温","降水","確率","風速","風向"];
 
-const labelsEl = document.createElement("div");
-labelsEl.className = "weather-labels";
+  const labelsEl = document.createElement("div");
+  labelsEl.className = "weather-labels";
 
-labels.forEach(text => {
-  const div = document.createElement("div");
-  div.className = "weather-label";
-  div.textContent = text;
-  labelsEl.appendChild(div);
-});
+  for (const text of labels) {
+    const div = document.createElement("div");
+    div.className = "weather-label";
+    div.textContent = text;
+    labelsEl.appendChild(div);
+  }
 
   const tableEl = document.createElement("div");
   tableEl.className = "weather-table";
