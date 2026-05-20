5fab33690f2cc23b9bbe17b750cb2a48aa1c2f09
4476barebare <aroe6295@gmail.com>
2026-05-21 08:03:03 +0900

    Add files via upload

--- js/dataLoader.js
+++ js/dataLoader.js
@@ -1226,7 +1226,7 @@ function createWeekItem(weekData) {
     ...dailyList.map(v => ({ type: "daily", data: v }))
   ].filter(v => v && v.data);
 
-  const labels = ["","","WEEK",""," TEMP","WAVE"];
+  const labels = ["","","WEEK","","","WAVE TEMP"];
 
   for (const text of labels) {
     const div = document.createElement("div");
