c5cd88d2911a3e25ba2530c2555e0cb8e19adda5
4476barebare <aroe6295@gmail.com>
2026-06-27 10:48:42 +0900

    Add files via upload

--- js/dataLoader.js
+++ js/dataLoader.js
@@ -1193,7 +1193,7 @@ function zoomToSpot(spot) {
 // ========================
 // データ補完
 // ========================
-let safe = { ...spot };
+let safe = spot;
 alert(safe.URL);
 // ★ special分岐（副作用なし）
 const typeParts = (safe.type || '').split('$');
