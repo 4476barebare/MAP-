9244f18a64487ef6686158460e226285b56a6d80
4476barebare <aroe6295@gmail.com>
2026-09-04 23:45:08 +0900

    Add files via upload

--- js/dataLoader.js
+++ js/dataLoader.js
@@ -728,7 +728,7 @@ function selectSpot(spot) {
 
     window.map.once('moveend', () => {
         window.map.invalidateSize(true);
-  
+     alert(window.currentAreaId);
         if (!window.areaBounds && window.currentAreaId) {
             alert("4");
             showSpotsForArea(window.currentAreaId);
